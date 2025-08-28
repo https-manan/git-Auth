1f3d6a2fcc6b132d7b51b6aacf47a1be5ac54b19
Manan <Mananbhardwaj2705@gmail.com>
2025-08-28 19:24:07 +0530

    updated backend

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { generateState, GitHub } = require('arctic');
+const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
 const port = 8080;
@@ -33,6 +34,52 @@ app.get('/',async (req,res)=>{
 })
 
 
+app.get('/github/callback',async (req,res)=>{
+    const {code,state} = req.query;
+    const cookieState = req.session.state;
+    if(state!=cookieState){
+        alert('Unauthorized aascess');
+    }
+    try {
+        const token = await gitHub.validateAuthorizationCode(code);
+    } catch (error) {
+        console.log(error)
+    }
+
+    const gitHubUserRes = await axios.get('https://api.github.com/user',{
+        headers:{
+            Authorization:`Bearer${token.ascessToken()}`
+        }
+    });
+    if(!gitHubUserRes.ok){alert('No response')}
+    const gitHubUser = gitHubUserRes.json();
+    const {id:gitHubUserId,name} = gitHubUser;
+
+    const gitHubEmailRes = await axios.get('https://api.github.com/user/emails', {
+    headers: {
+        Authorization: `Bearer ${token.accessToken()}`
+    }
+  });
+
+    const emails = gitHubEmailRes.data; 
+    const primaryEmail = emails.find(e => e.primary)?.email;
+
+    //3 conditions
+    //1st when the user exists and is registed with gitAUth
+    const user = await getUserWithOauthId({
+        provider:'github',
+        email,
+    });
+
+    //2nd when the user exists but not registered with gitAuth
+    if(user && !user.providerAccountId){
+        await linkUserWithOauth({
+            userId:user.id,
+            provider:'github',
+            providerAccountId:gitHubUserId
+        })
+    }
+})
 
 
 app.listen(port,()=>{
